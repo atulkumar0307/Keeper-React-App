2a7aa0c4c09ebef984174034e05e19b0269e5091
Atul Kumar <157139213+atulkumar0307@users.noreply.github.com>
2024-05-31 15:31:13 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from "./components/App";
 
 const root = createRoot(document.getElementById('root'));
-// const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
